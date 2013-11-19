84658cadfa7d6d8d83413953868d9491d94b36c3
Morgan BRUNOT <mbrunot.externe@m6.fr>
2013-11-19 14:45:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# JenkinsLight
+
+A build monitoring tool (buildwall) that allows you to quickly detect failing projects for Jenkins.
+
+### Requirements
+
+This project required a [cors-plugin Jenkins plugin](https://github.com/jhinrichsen/cors-plugin) to enabled CORS.  
+Enabling CORS would let you call the Jenkins REST API from javascript (you can use [the provided packaged plugin](bin/cors.hpi)).
+
+### Installation
+
+```shell
+$ git clone git@github.com:M6Web/JenkinsLight.git
+$ cd JenkinsLight
+$ bower install
+```
+
+### Configuration
+
+Please configure a new `js/config.js` file from [`js/config.js.dist`](js/config.js.dist).
+
+| Option | Description |
+|--------|-------------|
+| JENKINS_URL | Jenkins server url |
+| AUTHORIZATION_TOKEN | If your jenkins server is secured, set your authoriaztion token like "Basic 0123456=" (optional) |
+| DEFAULT_JENKINS_VIEW | Default view. Like "All"|
+| JOBS_TO_BE_DISPLAYED | Array of all jobs types that can be displayed |
+| DEFAULT_JOBS_PER_LINE | Default number of jobs per line on a screen |
+| REFRESH_TIME | Refresh time |
+
+### Use
+
+Use `view` query parameter for select a Jenkins view.
+
+```
+http://jenkins-light-url/index.html#?view=MyView
+```
+
+### Credits
+
+Developped by the [Cytron Team](http://cytron.fr/) of [M6 Web](http://tech.m6web.fr/).
+
+### License
+
+[JenkinsLight](https://github.com/M6Web/JenkinsLight) is licensed under the [MIT license](LICENSE).
---
+++ js/app.js
@@ -0,0 +1,60 @@
+angular.
+    module("JenkinsLightApp", ["config"]).
+    config(function($httpProvider, CONFIG) {
+        if (CONFIG.AUTHORIZATION_TOKEN) {
+
+            // Set authorization token (http://en.wikipedia.org/wiki/Basic_access_authentication#Client_side)
+            $httpProvider.defaults.headers.common['Authorization'] = CONFIG.AUTHORIZATION_TOKEN;
+        }
+    }).
+    controller("JenkinsLightCtrl", function JenkinsLightCtrl ($scope, CONFIG, $http, $interval, $location) {
+        $scope.jobs        = [];
+        $scope.jobsPerLine = CONFIG.DEFAULT_JOBS_PER_LINE;
+
+        var viewParameter = CONFIG.DEFAULT_JENKINS_VIEW;
+        if ($location.search().view) {
+
+            // Set the value of the view query parameter
+            viewParameter = $location.search().view;
+        }
+
+        var callAPI = function () {
+
+            // Call Jenkins API
+            $http({method: "GET", url: CONFIG.JENKINS_URL + "/view/" + viewParameter + "/api/json"}).
+                success(function(data) {
+                    $scope.jobs= [];
+
+                    data.jobs.forEach(function(job) {
+
+                        // Check if this `job` can be displayable
+                        if (CONFIG.JOBS_TO_BE_DISPLAYED.indexOf(job.color) > -1) {
+                            job.name = job.name.
+                                split('-').join(' ').
+
+                                // Remove all occurrence of view name in `job` name
+                                split(new RegExp(viewParameter, "gi")).join('');
+
+                            // Push job on screen
+                            $scope.jobs.push(job);
+                        }
+                    });
+
+                    // Set the number of job per line
+                    if ($scope.jobs.length <= 4) {
+                        $scope.jobsPerLine = $scope.jobs.length;
+                    } else if (($scope.jobs.length % 5) == 0 && $scope.jobs.length >= 30) {
+                        $scope.jobsPerLine = 5;
+                    } else if (($scope.jobs.length % 4) == 0) {
+                        $scope.jobsPerLine = 4;
+                    } else if (($scope.jobs.length % 3) == 0) {
+                        $scope.jobsPerLine = 3;
+                    }
+                });
+        };
+
+        callAPI();
+
+        // Begin interval
+        $interval(callAPI, CONFIG.REFRESH_TIME);
+    });
