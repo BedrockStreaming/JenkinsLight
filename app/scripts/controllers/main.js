2e9d6058539ab833d80c859ef1a1da0382fb8dbb
julien.bianchi <julien.bianchi@pmsipilot.com>
2013-11-26 15:07:58 +0100

    Quelques tweak rapides

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,54 +1,50 @@
 'use strict';
 
 angular.module('jenkinsLightApp')
-    .controller('JenkinsLightCtrl', function JenkinsLightCtrl ($scope, CONFIG, $http, $interval, $location) {
-        $scope.jobs        = [];
+    .controller('JenkinsLightCtrl', function JenkinsLightCtrl ($scope, CONFIG, $http, $timeout, $location) {
         $scope.jobsPerLine = CONFIG.DEFAULT_JOBS_PER_LINE;
 
-        var viewParameter = CONFIG.DEFAULT_JENKINS_VIEW;
-        if ($location.search().view) {
+        var viewParameter = $location.search().view ? $location.search().view : CONFIG.DEFAULT_JENKINS_VIEW,
+            fetchView = function(viewName, url) {
+                var cleanName = viewName.replace(/\/view\//gi, '/');
 
-            // Set the value of the view query parameter
-            viewParameter = $location.search().view;
-        }
+                if($scope.views.hasOwnProperty(cleanName) === false) {
+                    $scope.views[cleanName] = { realname: viewName, jobs: {} };
+                }
 
-        var callAPI = function () {
+                $http({method: 'GET', url: url}).
+                    success(function(data) {
+                        if(data.views) {
+                            data.views.forEach(function(view) {
+                                fetchView(viewName + '/view/' + view.name, view.url + 'api/json');
+                            });
+                        } else {
+                            data.jobs.forEach(function(job) {
+                                // Check if this `job` can be displayable
+                                if (CONFIG.JOBS_TO_BE_DISPLAYED.indexOf(job.color) > -1) {
+                                    job.name = job.name.
+                                        split('-').join(' ').
+                                        split('_').join(' ').
+                                        split('.').join(' ').
+                                        // Remove all occurrence of view name in `job` name
+                                        split(new RegExp(viewParameter, 'gi')).join('');
 
-            // Call Jenkins API
-            $http({method: 'GET', url: CONFIG.JENKINS_URL + '/view/' + viewParameter + '/api/json'}).
-                success(function(data) {
-                    $scope.jobs= [];
-
-                    data.jobs.forEach(function(job) {
-
-                        // Check if this `job` can be displayable
-                        if (CONFIG.JOBS_TO_BE_DISPLAYED.indexOf(job.color) > -1) {
-                            job.name = job.name.
-                                split('-').join(' ').
-
-                                // Remove all occurrence of view name in `job` name
-                                split(new RegExp(viewParameter, 'gi')).join('');
-
-                            // Push job on screen
-                            $scope.jobs.push(job);
+                                    $scope.views[cleanName].jobs[job.name] = job;
+                                }
+                            });
                         }
                     });
+            },
+            callAPI = function () {
+                fetchView(viewParameter, CONFIG.JENKINS_URL + '/view/' + viewParameter + '/api/json');
 
-                    // Set the number of job per line
-                    if ($scope.jobs.length <= 4) {
-                        $scope.jobsPerLine = $scope.jobs.length;
-                    } else if (($scope.jobs.length % 5) === 0 && $scope.jobs.length >= 30) {
-                        $scope.jobsPerLine = 5;
-                    } else if (($scope.jobs.length % 4) === 0) {
-                        $scope.jobsPerLine = 4;
-                    } else if (($scope.jobs.length % 3) === 0) {
-                        $scope.jobsPerLine = 3;
-                    }
-                });
+                $timeout(callAPI, CONFIG.REFRESH_TIME);
+            };
+
+        $scope.views = {};
+        $scope.viewCount = function(name) {
+            return Object.keys($scope.views[name].jobs).length;
         };
 
         callAPI();
-
-        // Begin interval
-        $interval(callAPI, CONFIG.REFRESH_TIME);
     });
