44d504a8bb4bb0151d1d58b99f0212b782439792
Morgan Brunot <brunot.morgan@gmail.com>
2014-01-16 21:55:07 +0100

    remove unused  and  services

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('jenkinsLightApp')
-    .controller('JenkinsLightCtrl', function JenkinsLightCtrl ($scope, CONFIG, JenkinsService, $http, $interval, $location) {
+    .controller('JenkinsLightCtrl', function JenkinsLightCtrl ($scope, CONFIG, JenkinsService, $interval) {
         $scope.jobs        = [];
         $scope.jobsPerLine = CONFIG.DEFAULT_JOBS_PER_LINE;
 
