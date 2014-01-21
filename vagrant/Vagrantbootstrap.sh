56ec554649fd798d319e00742a8369f9b5f2de44
Florent Dubost <florent.dubost@gmail.com>
2014-01-21 15:55:21 +0100

    Add git

--- vagrant/Vagrantbootstrap.sh
+++ vagrant/Vagrantbootstrap.sh
@@ -4,8 +4,10 @@ sudo apt-get update
 sudo apt-get -y install python-software-properties
 
 sudo add-apt-repository ppa:chris-lea/node.js
-
 sudo apt-get update
+
+sudo apt-get -y install build-essential 
+sudo apt-get -y install git
 sudo apt-get -y install nodejs
 
 sudo npm install -g grunt-cli bower
